80843cfd59fe7374b045fb31854713a4b29b26d6
Jyothish <117960824+Luf1o@users.noreply.github.com>
2024-03-07 11:29:49 +0530

    minor changes
    Deleted redundant codes

--- app/components/about/TeamAbout.jsx
+++ app/components/about/TeamAbout.jsx
@@ -4,22 +4,9 @@ import TeamCard from "../common/TeamCard";
 function TeamAbout(){
     const [selected,setSelected] = useState(null);
     function sliderOnClick(buttonId){
-        switch(buttonId){
-                case 1:{
-                    if(setSelected!=buttonId){
-                        setSelected(buttonId);
-                        console.log("selected")
-                }
-                    else{
-                        setSelected(null);
-                        console.log("unselected button 1")
-                    }
-                    break;
-                }
-                default: 
-                 console.log("error swtiching")
-                 break;
-        }
+       if(buttonId!=selected){
+            setSelected(buttonId)
+       }console.log(buttonId)
     }
     function popup(){
         console.log('Popup shown')
@@ -33,8 +20,8 @@ function TeamAbout(){
             </div> 
                 <TeamCard />
             <div className="flex w-full items-center justify-center gap-12 h-auto mt-6 p-1">
-                <div className="p-1 rounded-md w-32 h-3 bg-slate-300 hover:cursor-pointer "  onClick={sliderOnClick}></div>
-                <div className="p-1 rounded-md w-32 h-3 bg-slate-500"></div>
+                <div className="p-1 rounded-md w-32 h-3 hover:cursor-pointer 'selected ===buttonId? bg-slate-300 : bg-slate-500'  " id='dev' onClick={sliderOnClick}></div>
+                <div className="p-1 rounded-md w-32 h-3 bg-slate-500 hover:cursor-pointer " id='cap'  onClick={sliderOnClick}></div>
                 <div className="p-1 rounded-md w-32 h-3 bg-slate-500"></div>
                 <div className="p-1 rounded-md w-32 h-3 bg-slate-500"></div>
                 <div className="p-1 rounded-md w-32 h-3 bg-slate-500"></div>
